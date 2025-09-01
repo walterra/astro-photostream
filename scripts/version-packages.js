f49f496901976e2b5b0c53c5a58df7ddeacbb3fe
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-09-02 00:19:16 +0200

    ci: add version packages script and update workflow

---
+++ scripts/version-packages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+
+// Run changeset version
+execSync('changeset version', { stdio: 'inherit' });
+
+// Read the package.json to get the new version
+const packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
+const version = packageJson.version;
+
+// Stage and commit with conventional commit format
+execSync('git add .', { stdio: 'inherit' });
+execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' });
